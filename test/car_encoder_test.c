3ccb0131fe6040a87f0a1027cdd2cbeeb19e7589
xww <2775295957@qq.com>
2026-03-04 20:17:38 +0800

    修改编码器测试代码

--- test/car_encoder_test.c
+++ test/car_encoder_test.c
@@ -6,8 +6,7 @@ void car_encoder_test_proc(void) {
     // 测试编码器
     uint32_t current_time = get_current_ms();
     if (current_time - last_show_info_time > SHOW_INFO_INTERVAL) {
-        printf("car_L_encoder_count: %lld\r\n", car_encoder_get_L());
-        printf("car_R_encoder_count: %lld\r\n", car_encoder_get_R());
+        printf("%d,%d\r\n", (int32_t) car_encoder_get_L(), (int32_t) car_encoder_get_R());
         last_show_info_time = current_time;
     } else if (current_time < last_show_info_time) { // 处理计时器溢出的情况
         last_show_info_time = current_time;
