c07afb9b4886106cf0d8deba61f4657d0334cd4f
xww <2775295957@qq.com>
2026-03-22 00:09:50 +0800

    优化串口接收不定长数据的逻辑

--- User/car_usart2.c
+++ User/car_usart2.c
@@ -36,10 +36,11 @@ void usart2_init(void) {
 	USART_Init(USART2, &usart2_init_structure);
 	GPIO_PinRemapConfig(GPIO_Remap_USART2, DISABLE);
 	USART_ITConfig(USART2, USART_IT_RXNE, ENABLE); 
+	USART_ITConfig(USART2, USART_IT_IDLE, ENABLE); // 开启IDLE中断
 
 	NVIC_InitTypeDef nvic_init_structure;
 	nvic_init_structure.NVIC_IRQChannel = USART2_IRQn;
-	nvic_init_structure.NVIC_IRQChannelPreemptionPriority = 0;
+	nvic_init_structure.NVIC_IRQChannelPreemptionPriority = 2;
 	nvic_init_structure.NVIC_IRQChannelSubPriority = 0;
 	nvic_init_structure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&nvic_init_structure);
@@ -57,9 +58,10 @@ void USART2_IRQHandler(void) {
 	if (USART_GetITStatus(USART2, USART_IT_RXNE) == SET) {
 		receive_buff[receive_idx] = USART_ReceiveData(USART2);
 		receive_idx ++;
-		if (receive_idx > 2 && receive_buff[receive_idx - 2] == '\r' && receive_buff[receive_idx - 1] == '\n') { // 说明接收数据结束
-			send_string((char *) receive_buff, receive_idx - 2);
-			receive_idx = 0;
-		}
+	}
+	if (USART_GetITStatus(USART2, USART_IT_IDLE) == SET) { // 接收到IDLE中断 说明数据接收完毕
+		USART_ClearITPendingBit(USART2, USART_IT_IDLE); // 清除IDLE中断 
+		send_string((char *) receive_buff, receive_idx);
+		receive_idx = 0;
 	}
 }
