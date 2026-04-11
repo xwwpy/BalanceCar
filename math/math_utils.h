8703ac6738713ed5a2353775ff9069be415cfdab
xww <2775295957@qq.com>
2026-04-12 02:36:34 +0800

    新增快速三角函数计算函数

--- math/math_utils.h
+++ math/math_utils.h
@@ -1,6 +1,14 @@
 #ifndef __MATH_UTILS_H__
 #define __MATH_UTILS_H__
 #include "stm32f10x.h"
+
 uint32_t Xww_pow(uint8_t base, uint8_t exp);
 uint32_t Xww_abs(int32_t val);
+float qsin(float x);
+float qcos(float x);
+float qtan(float x);
+float qasin(float x);
+float qacos(float x);
+float qatan(float x);
+float qatan2(float y, float x);
 #endif
